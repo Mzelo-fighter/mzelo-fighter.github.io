1e37af246c61a1a2997a15adf67f4280f45f6125
Mzelo-fighter <mzelo-fighter@proton.me>
2026-07-24 03:17:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener(
     "DOMContentLoaded",
     () => {
         console.log(
-            "I really appreciate everyone that actually cared. Thank you for everything Mzelo. I hope you guys can become a better community than it currently is. Goodbye Mzello - Fighter"
+            "Thank you for everything Mzelo. I really appreciate everyone that actually cared about what I felt. I hope you guys can become a better community than it currently is. Goodbye Mzello - Fighter"
         );
     }
 );
