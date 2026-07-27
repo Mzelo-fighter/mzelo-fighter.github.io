38db5bea75ad75cca4630805ffa97c6e92b0797f
Mzelo-fighter <mzelo-fighter@proton.me>
2026-07-27 22:45:42 +0000

    Update appreciation message for community feedback

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener(
     "DOMContentLoaded",
     () => {
         console.log(
-            "I really appreciate the one person that actually cared. Thank you for everything. I hope you guys can become a better community than it currently is. Goodbye Mzello - From Fighter"
+            "I really appreciate the one person that actually cared. Thank you for everything. I hope you guys can become a better community than its current toxic state. Goodbye Mzello - From Fighter"
         );
     }
 );
