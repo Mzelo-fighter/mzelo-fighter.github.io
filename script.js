eda4f9df730b323a78e9ec9d310d052e12c4687b
Mzelo-fighter <mzelo-fighter@proton.me>
2026-07-27 22:36:27 +0000

    Update farewell message in script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener(
     "DOMContentLoaded",
     () => {
         console.log(
-            "Thank you for everything Mzelo. I really appreciate everyone that actually cared about what I felt. I hope you guys can become a better community than it currently is. Goodbye Mzello - Fighter"
+            "I really appreciate the one person that actually cared. Thank you for everything. I hope you guys can become a better community than it currently is. Goodbye Mzello - From Fighter"
         );
     }
 );
